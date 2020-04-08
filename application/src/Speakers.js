ddaaaa2573d22a0dd9c3c01c42b8d9ff467e45ef
unknown <gustavoaraujo600@hotmail.com>
2020-04-08 11:44:54 -0300

    using useReducer

--- application/src/Speakers.js
+++ application/src/Speakers.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { useState, useEffect, useContext, useReducer } from "react";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../static/site.css";
@@ -8,11 +8,16 @@ import SpeakerData from "./SpeakerData";
 import SpeakerDetail from "./SpeakerDetail";
 import { ConfigContext } from './App';
 
+import speakersReducer from './speakersReducer';
+
 const Speakers = ({ }) => {
   const [speakingSaturday, setSpeakingSaturday] = useState(true);
   const [speakingSunday, setSpeakingSunday] = useState(true);
+  // const [speakerList, setSpeakerList] = useState([]);
 
-  const [speakerList, setSpeakerList] = useState([]);
+
+  const [speakerList, dispatch] = useReducer(speakersReducer, []);
+
   const [isLoading, setIsLoading] = useState(true);
 
   const context = useContext(ConfigContext);
@@ -28,7 +33,11 @@ const Speakers = ({ }) => {
       const speakerListServerFilter = SpeakerData.filter(({ sat, sun }) => {
         return (speakingSaturday && sat) || (speakingSunday && sun);
       });
-      setSpeakerList(speakerListServerFilter);
+      //setSpeakerList(speakerListServerFilter);
+      dispatch({
+        type: "setSpeakerList",
+        data: speakerListServerFilter
+      });
     });
     return () => {
       console.log("cleanup");
@@ -62,14 +71,17 @@ const Speakers = ({ }) => {
   const heartFavoriteHandler = (e, favoriteValue) => {
     e.preventDefault();
     const sessionId = parseInt(e.target.attributes["data-sessionid"].value);
-    setSpeakerList(speakerList.map(item => {
-      if (item.id === sessionId) {
-        item.favorite = favoriteValue;
-        return item;
-      }
-      return item;
-    }));
-    //console.log("changing session favorte to " + favoriteValue);
+    dispatch({
+      type: favoriteValue === true ? "favorite" : "unfavorite",
+      sessionId
+    });
+    // setSpeakerList(speakerList.map(item => {
+    //   if (item.id === sessionId) {
+    //     item.favorite = favoriteValue;
+    //     return item;
+    //   }
+    //   return item;
+    // }));
   };
 
   if (isLoading) return <div>Loading...</div>;
